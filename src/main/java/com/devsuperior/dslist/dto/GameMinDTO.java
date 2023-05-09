83ec8534429e454a8e5e573849bfbb2a1b11ed18
Eduardo Serafim <eduserafim7@gmail.com>
2023-05-09 16:24:59 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+import jakarta.persistence.Column;
+
+public record GameMinDTO(
+        Long id,
+        String title,
+        Integer year,
+        String imgUrl,
+        String shortDescription
+) {
+    public GameMinDTO(Game entity){
+        this(entity.getId(), entity.getTitle(), entity.getYear(), entity.getImgUrl(), entity.getShortDescription());
+    }
+}
