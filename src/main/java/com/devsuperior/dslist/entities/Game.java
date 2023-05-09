2f8a3d94009930a1ddc5ff34c4b64a2e1e823c0e
Eduardo Serafim <eduserafim7@gmail.com>
2023-05-09 15:58:07 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,10 +20,15 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
 
-    private String  genre;
-    private String platform;
+    private String genre;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT") 
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 }
