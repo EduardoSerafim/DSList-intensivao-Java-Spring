2d6badd0f0ecdf5eaa137f966e7170a44ccc8f4e
Eduardo Serafim <eduserafim7@gmail.com>
2023-05-10 11:59:06 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public record GameListDTO(
+        Long id,
+        String name
+) {
+    public GameListDTO(GameList entity){
+        this(entity.getId(), entity.getName());
+    }
+}
